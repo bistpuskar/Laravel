dcab6d800b812a62503671cdb341b007e8e85456
Puskar <bistpuskar123@gmail.com>
2019-10-20 22:33:28 +0545

    conflict solved

--- resources/views/admin/user/add.blade.php
+++ resources/views/admin/user/add.blade.php
@@ -14,23 +14,7 @@
             </div>
             <!-- /.box-header -->
             <!-- form start -->
-<<<<<<< HEAD
-            <form method="POST" action="{{'admin.user.store'}}" class="form-horizontal">
-               {{ csrf_field() }}
-              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-=======
             <form method="POST" action="{{URL::route('admin.user.store')}}" class="form-horizontal">
                {{ csrf_field() }}
               <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -46,7 +30,6 @@
                         @endif
                     </div>
                 </div>
->>>>>>> ba4c13c8a26c45b29a0673784939dcba3e4c3347
 
               <div class="box-footer">
                 <input type="submit" class="btn btn-primary" value="submit">
